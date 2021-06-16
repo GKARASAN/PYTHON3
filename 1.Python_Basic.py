7590efb27fbbd147f9641a789db1b526529704ad
KALAIYARASAN.G <85977784+GKARASAN@users.noreply.github.com>
2021-06-16 15:15:32 +0530

    Add files via upload

---
+++ 1.Python_Basic.py
@@ -0,0 +1,11 @@
+        #! don't run this
+
+# creator - Guido Van Rossum
+# release in 1991
+
+""" python is object oriented programming system , it is case sensitive language"""
+
+''' used in Web Development (Server-side) , Softare Development , Mathematics and System Scripting'''
+
+# easy to learn
+# popular language
