08a151e3d849ed73a8559a5de5ec3bc7fdaa833c
KALAIYARASAN.G <85977784+GKARASAN@users.noreply.github.com>
2021-06-18 19:03:05 +0530

    Update 6.Type_Casting.py

--- 6.Type_Casting.py
+++ 6.Type_Casting.py
@@ -3,5 +3,13 @@ print(str(5))    #! type int to str changed
 print(float(5))  #! type int to float changed
 print(bool(5))   #! type int to bool changed
 
+"""
 ''' you can change in any type str , int , float , bool and more type's '''
 
+Casting in python is therefore done using constructor functions:
+
+int()   - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
+float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
+str()   - constructs a string from a wide variety of data types, including strings, integer literals and float literals
+
+"""
