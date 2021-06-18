872e47a1561a556536be0289853d7822fc64cc4b
KALAIYARASAN.G <85977784+GKARASAN@users.noreply.github.com>
2021-06-18 18:46:18 +0530

    Update 3.Variable.py

--- 3.Variable.py
+++ 3.Variable.py
@@ -1,7 +1,15 @@
 variable = "Hello World!"
 print(variable)    #! value's can store on variable continor
 
-''' variable rules is , Start in latters or _ under source , dont use spl characters'''
+"""
+Variable Names
+   1. A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+   2. A variable name must start with a letter or the underscore character
+   3. A variable name cannot start with a number
+   4. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+   5. Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+"""
 
            #! index
 
